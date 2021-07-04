45ede2bfd728f899bcb6e60170cfeec02590b35f
shuxinhuan <vlanxin11@gmail.com>
2021-07-04 15:05:20 +0800

    update overview

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import ant from "./utils/ant"
-
-createApp(App)
-    .use(store)
-    .use(ant)
-    .mount('#app')
